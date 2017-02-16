02080ad495ed39618206a1a82385626e8ff1ae2a
J-Veber <MammaMoo_13>
2017-02-16 23:33:48 +1000

    create template for other branches

--- classes/views/main.blade.php
+++ classes/views/main.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    @include('head.blade.php')
+    @include('head')
 </head>
 <body>
 <section class="todoapp">
@@ -9,7 +9,14 @@
         <h1>todos</h1>
         <input class="new-todo" placeholder="What needs to be done?" autofocus>
     </header>
+
     <!-- This section should be hidden by default and shown when there are todos -->
+    //если нет куки
+    @section('login')
+        @show
+    //иначе
+    @section('content')
+        @show
 
     <!-- This footer should hidden by default and shown when there are todos -->
 
@@ -18,7 +25,7 @@
     <p>Double-click to edit a todo</p>
 </footer>
 <!-- Scripts here. Don't remove ↓ -->
-<script src="node_modules/todomvc-common/base.js"></script>
-<script src="js/app.js"></script>
+{{--<script src="node_modules/todomvc-common/base.js"></script>--}}
+{{--<script src="js/app.js"></script>--}}
 </body>
 </html>
