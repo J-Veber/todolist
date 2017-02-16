8488e0867d4404321b47b09d24080079a754361e
J-Veber <MammaMoo_13>
2017-02-16 23:31:42 +1000

    create authentication branch

---
+++ classes/views/authentication.blade.php
@@ -0,0 +1,13 @@
+@extends('main')
+
+//если нажата кнопка "забыли пароль?"
+@section('restore_password')
+    @show
+
+//если нажата кнопка "регистрация"
+@section('registration')
+    @show
+
+//иначе
+@section('login')
+    @show
