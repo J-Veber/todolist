21d185893031e15b8b804ca9361515f325c2418c
J-Veber <MammaMoo_13>
2017-02-17 19:13:46 +1000

    rename main function

--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-  'todos' => 'main/index', //render content
+  'todos' => 'content/EmptyIndex', //render content
     'registration' => 'registration/index',
     'authorization' => 'authorization/index',
     'reset_passw' => 'reset/index'
