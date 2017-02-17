c09a681b45d7831087bae38724f58c914787da4e
J-Veber <MammaMoo_13>
2017-02-17 17:32:46 +1000

    review code

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <?php
 
+//общие настройки
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 include ('config.php');
 include ('classes/registry.php');
 include ('classes/router.php');
 
-require_once "vendor/autoload.php";
+require_once 'vendor/autoload.php';
 
 use Classes\Router;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+
+
+//содеинение с БД
+
 
 (new Router(null))->start();
 die();
@@ -33,7 +41,6 @@ $router->setPath(SITE_PATH . 'controllers' . '/');
 
 echo SITE_PATH;
 $router->start();
-
 
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
 $entityManager = EntityManager::create($dbParams, $config);
