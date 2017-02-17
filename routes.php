3c28a20c987040fac30162d396540bcc23379e86
J-Veber <MammaMoo_13>
2017-02-17 17:33:14 +1000

    create routes.php

---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+  'todos' => 'main/index', //render content
+    'registration' => 'registration/index',
+    'authorization' => 'authorization/index',
+    'reset_passw' => 'reset/index'
+);
