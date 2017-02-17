4314c5988f2790a2550313d1355cbabcd9dc74b1
J-Veber <MammaMoo_13>
2017-02-17 19:09:46 +1000

    render view

--- controllers/AuthorizationController.php
+++ controllers/AuthorizationController.php
@@ -1,9 +1,15 @@
 <?php
 include_once "BaseController.php";
+use duncan3dc\Laravel\BladeInstance;
+
 class AuthorizationController extends BaseController
 {
     function actionIndex()
     {
+        //echo 'In AuthorizationController';
+        $blade = new BladeInstance(__DIR__ . "/views", __DIR__ . "/views");
+        echo $blade->render("login");
+        return true;
         // TODO: Implement actionIndex() method.
     }
 }
