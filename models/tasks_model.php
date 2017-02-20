da635ce25d1edaef84a2996bee53a0f002c88167
J-Veber <MammaMoo_13>
2017-02-20 17:54:49 +1000

    rename to Tasks_Model

--- models/tasks_model.php
+++ models/tasks_model.php
@@ -1,8 +1,10 @@
 <?php
-class TaskModel extends BaseModel
+use baseModel\Base_Model;
+
+class Tasks_Model extends Base_Model
 {
     public $_task_id;
-    public $_user_login;
+    public $_user_id;
     public $_task_text;
     public $task_done;
 
@@ -10,8 +12,8 @@ class TaskModel extends BaseModel
     {
         return array(
             '_task_id' => 'id',
-            '_user_login' => 'login',
-            '_task_text' => 'text',
+            '_user_id' => 'user_id',
+            '_task_text' => 'task_content',
             '_task_done' => 'done'
         );
     }
