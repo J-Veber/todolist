79fde9d71042953ecf540ddf00cf3641086c629d
J-Veber <MammaMoo_13>
2017-02-22 00:32:26 +1000

    create login reaction on button

--- controllers/AuthorizationController.php
+++ controllers/AuthorizationController.php
@@ -1,11 +1,37 @@
 <?php
 include_once "BaseController.php";
 use duncan3dc\Laravel\BladeInstance;
+use \Delight\Auth;
+
 class AuthorizationController extends BaseController
 {
     function actionIndex()
     {
-        //echo 'In AuthorizationController';
+        echo "YO";
+//        if (isset($_POST['submit']))
+//        {
+//            $auth = new \Delight\Auth\Auth(DB::class);
+//
+//            try
+//            {
+//                $md5passw = md5(md5($_POST['password']));
+//                $auth->login($_POST['email'], $md5passw);
+//                echo "SUCCSESS";
+//            } catch (Auth\InvalidEmailException $e)
+//            {
+//                echo "wrong email address";
+//            } catch (Auth\InvalidPasswordException $e)
+//            {
+//                echo "wrong password";
+//            } catch (Auth\TooManyRequestsException $e)
+//            {
+//                echo "too many requests";
+//            }
+//        }
+    }
+
+    private function view()
+    {
         $blade = new BladeInstance(__DIR__ . "/views", __DIR__ . "/views");
         echo $blade->render("login");
         return true;
