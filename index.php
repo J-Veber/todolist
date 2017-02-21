f2711360c626d28aebd651c8562d62ca7357dfd9
J-Veber <MammaMoo_13>
2017-02-22 00:39:10 +1000

    add new DB, create DB connection and model tests

--- index.php
+++ index.php
@@ -11,16 +11,27 @@ include ('classes/router.php');
 require_once 'meekrodb.2.3.class.php';
 require_once 'vendor/autoload.php';
 use Classes\Router;
+use DB;
 use MeekroDB;
+use Users_Model;
 
-use userModel\Users_Model;
-
+include ('models/users_model.php');
 
 //содеинение с БД
 $port = '8000';
 
-$mdb = new MeekroDB(DB_HOST, DB_USER, DB_PASS, DB_NAME, $port, 'utf8');
+//$db = new DB();
+DB::$user = DB_USER;
+DB::$password = DB_PASS;
+DB::$dbName = DB_NAME;
+DB::$host = DB_HOST; //defaults to localhost if omitted
+DB::$port = $port; // defaults to 3306 if omitted
+DB::$encoding = 'utf8'; // defaults to latin1 if omitted
 
+
+//$users = new Users_Model('4', 'anny', 'azaza', 'kokoko@gmail.com');
+//$users->save();
+//echo $users->getRowById('1');
 (new Router())->start();
 //echo "YO";
 //$row = $mdb->queryFirstRow("SELECT name, email FROM users");
