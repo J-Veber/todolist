4e8f555feab8fd7265fa6f482d6fab90442eb18a
J-Veber <MammaMoo_13>
2017-02-22 23:46:27 +1000

    new bramus Router

--- classes/router.php
+++ classes/router.php
@@ -1,70 +1,42 @@
 <?php
 namespace Classes;
-use duncan3dc\Laravel\BladeInstance;
-use Exception;
-use Klein\Klein;
-use Klein\Route;
-use FastRoute;
+
+include (SITE_PATH.'controllers/AuthorizationController.php');
+include (SITE_PATH.'controllers/ContentController.php');
+include (SITE_PATH.'controllers/MainController.php');
+include (SITE_PATH.'controllers/RegistrationController.php');
+include (SITE_PATH.'controllers/ResetController.php');
+
+
+use Controllers\AuthorizationController;
+use Controllers\ContentController;
+use MainController;
+use RegistrationController;
+use ResetController;
+
 class Router
 {
-    private $registry;
-    private $path;
-    private $args = array();
-    private $routes;
-    function __construct()
-    {
-        $routesPath = SITE_PATH.'/routes.php';
-        $this->routes = include($routesPath);
+    private function __construct() {
+        $this->bramusRouter = new \Bramus\Router\Router();
+        $this->autorizationController = new AuthorizationController();
+        $this->contentController = new ContentController();
+        $this->mainController = new MainController();
+        $this->resetController = new ResetController();
+        $this->registrationController = new RegistrationController();
     }
-    //путь до папки с контроллерами
-    function setPath($path)
-    {
-        $path = trim($path, '\\');
-        $path .= DS;
-        if (is_dir($path) == false) {
-            throw new Exception ('Invalid controller path: `' . $path . '`');
-        }
-        $this->path = $path;
-    }
-    //returns request string
-    private function getURI()
-    {
-        if (!empty($_SERVER['REQUEST_URI']))
-        {
-            return trim($_SERVER['REQUEST_URI'], '/');
-        }
-    }
-    function start()
-    {
-        //получить строку маршрута
-        $uri = $this->getURI();
-        //echo $uri;
-        //поиск маршрута
-        foreach ($this->routes as $uriPattern => $path)
-        {
-            if (preg_match("~$uriPattern~" , $uri))
-            {
-                $segments = explode('/', $path);
-                $controllerName = array_shift($segments) . "Controller";
-                $controllerName = ucfirst($controllerName);
-                $actionName = 'action' . ucfirst(array_shift($segments));
-                //Подключить файл класса контроллера
-                $controllerFile = SITE_PATH . '/controllers/' .
-                    $controllerName . '.php';
-                //echo "<br>$controllerFile";
-                //echo "<br>$controllerName";
-                if (file_exists($controllerFile))
-                {
-                    include_once ($controllerFile);
-                }
-                //Создать объект, вызвать метод
-                $controllerObject = new $controllerName;
-                $result = $controllerObject->$actionName();
-                if ($result != null)
-                {
-                    break;
-                }
-            }
-        }
+
+    public static function start() {
+        $router = new self();
+        $bramusRouter = $router->bramusRouter;
+
+        $bramusRouter->get('/[authorization]', $router->mainController->actionIndex());
+        $bramusRouter->get('/todos', $router->contentController->actionIndex());
+        $bramusRouter->get('/reset_passw', $router->resetController->actionIndex());
+        $bramusRouter->get('/registration', $router->registrationController->actionIndex());
+
+
+
+
+        $bramusRouter->run();
     }
 }
