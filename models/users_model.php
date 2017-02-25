c7d057a7ce7ff7522453388b66fdae1669d80069
J-Veber <MammaMoo_13>
2017-02-26 04:52:16 +1000

    add find user with login+passw and email

--- models/users_model.php
+++ models/users_model.php
@@ -22,6 +22,9 @@ class Users_Model extends Base_Model
         $this->_user_email = $email;
     }
 
+    /**
+     * @return string
+     */
     public function save()
     {
         try
@@ -34,10 +37,9 @@ class Users_Model extends Base_Model
             ));
         } catch(MeekroDBException $e)
         {
-            echo 'Error : ' . $e->getMessage();
-            echo '<br/>Error sql : ' . "'INSERT INTO $this->_table (id, name, passw, email) VALUES (*some values)'";
-            exit();
+            return 'Error : ' . $e->getMessage() . '<br/>Error sql : ' . "'INSERT INTO $this->_table (id, name, passw, email) VALUES (*some values)'";
         }
+        return true;
     }
 
     public function updatePassw($newpassw)
@@ -80,6 +82,30 @@ class Users_Model extends Base_Model
             echo 'Error : ' . $e->getMessage();
             echo '<br/>Error sql : ' . "FIND user WHERE id = $id";
             exit();
+        }
+    }
+
+    public function findByLoginAndPassw($login, $passw)
+    {
+        try
+        {
+            $mysqli_result = DB::query("SELECT * FROM $this->_table WHERE $login=%s" , $login, "$passw=%s", md5(md5($passw)));
+            return $mysqli_result;
+        } catch (MeekroDBException $e)
+        {
+            return 'Error : ' . $e->getMessage() . "<br/>Error sql : " . "FIND user WHERE login = $login and passw = $passw";
+        }
+    }
+
+    public function findByEmail($email)
+    {
+        try
+        {
+            $mysqli_result = DB::query("SELECT * FROM $this->_table WHERE $email=%s" , $email);
+            return $mysqli_result;
+        } catch (MeekroDBException $e)
+        {
+            return 'Error : ' . $e->getMessage() . "<br/>Error sql : " . "FIND user WHERE email = $email";
         }
     }
 
