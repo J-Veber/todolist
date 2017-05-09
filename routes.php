befb8e8840d5e4a0896dc774e8e1c5b142130576
veber <veber@veber-PC>
2017-05-09 21:52:58 +0000

    PS-2017.1 <veber@veber-PC Overwrite remote https://github.com/J-Veber/todolist.git to local

--- routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-  'todos' => 'content/index', //render content
-    'registration' => 'registration/index',
-    'authorization' => 'main/index',
-    'reset_passw' => 'reset/index'
-);
