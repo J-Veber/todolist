541d2e4a583c61ddebf064103ef919b1184fa12a
J-Veber <kovalsky.corp@gmail.com>
2017-05-18 14:57:24 +0000

    rewrite

--- App/Autoloader.php
+++ App/Autoloader.php
@@ -4,29 +4,36 @@ class Autoloader
     function __autoload($className)
     {
         $fileName = $className . '.php';
-        echo $fileName;
+//        echo $fileName . '<br/>';
         $fileArr = explode("\\", $fileName);
 
-        if (!empty($fileArr[0])){
+        if (!empty($fileArr[0])) {
             switch ($fileArr[0]) {
                 case 'TodoList':
                     // TODO: сделать переключение между моделями и контроллерами
                     if ($fileArr[1] == "Controllers") {
                         $folder = "src/Controllers";
+                        $class = $fileArr[2];
                     } else {
                         $folder = "src/Models";
+                        $classFile = explode("_", $fileArr[2]);
+                        $class = $classFile[0] . $classFile[1];
                     }
-                    $file = ROOT_PATH . $folder . DS . $fileArr[2];
+                    $file = ROOT_PATH . $folder . DS . $class;
                     require_once $file;
                     //echo $file ;
                     break;
-                case '':
+                //case '':
+                    /*echo "1234";
                     $folder = "App";
-                    $file = ROOT_PATH;
-                    echo $file;
+                    $file = ROOT_PATH . "../";
+                    $file = ROOT_PATH . $folder . DS . $fileArr[2];
+                    echo $file;*/
                 default:
-
-                    $folder = "vendor";
+                    $folder = "App";
+                    $file = ROOT_PATH . $folder . DS . "Core.php";
+                    require_once $file;
+                    //echo $file;
                     break;
             }
         } else {
@@ -36,62 +43,5 @@ class Autoloader
             //require_once $file;
         }
 
-
-
-        /*if (strpos($fileName, 'Controller.php')) {
-
-        }
-//        echo $fileName . "\n";
-        $expArr = explode(DS, $className); // TODO: хотим посмотреть какой там корневой неймспейс
-//        echo $expArr[0];
-        /* TODO: тут нужно разрешить подключение файлов с другими namespace */
-        /*if ($expArr[0] == 'TodoList') {
-            $file = str_replace(DS, '\\', $fileName);
-        } else {
-            $file = 'src' . DS . join('\\', array_slice($expArr, 1));
-        }
-
-        $file = ROOT_PATH . $file;
-        //echo $file . "\n";
-        if (!file_exists($file))
-        {
-            return false;
-        }
-        include ($file);*/
     }
-
 }
-
-
-
-    /*function spl_autoload_register($className)
-    {
-        $fileName = strtolower($className) . '.php';
-        $expArr = explode('_', $className);
-        if (empty($expArr[1]) OR $expArr[1] == 'Base')
-        {
-            $folder = 'classes';
-        } else
-        {
-            switch (strtolower($expArr[0]))
-            {
-                case 'controller':
-                    $folder = 'cotrollers';
-                    break;
-                case 'model':
-                    $folder = 'models';
-                    break;
-                default:
-                    $folder = 'classes';
-                    break;
-            }
-        }
-
-        $file = SITE_PATH . $folder . DS . $fileName;
-        if (!file_exists($file))
-        {
-            return false;
-        }
-        include ($file);
-        return true;
-    }*/
