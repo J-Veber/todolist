1aec6fa7d6d62b1fbaab14648c70734de80602e0
J-Veber <kovalsky.corp@gmail.com>
2017-05-19 17:01:47 +0000

    add api routes

--- App/routes.php
+++ App/routes.php
@@ -6,6 +6,15 @@ return array(
     'registration' => 'auth/registration',
     'login' => 'auth/login',
     'reset_passw' => 'auth/reset',
+
     'api/registration' => 'auth/registrationResponse',
-    'api/login' => 'auth/loginResponse'
+
+    'api/login' => 'auth/loginResponse',
+
+    'api/todos/addTask' => 'content/addTask',
+    'api/todos/returnAllTasks' => 'content/loadTasks',
+    'api/todos/removeTask' => 'content/removeTask',
+    'api/todos/removeAllCompletedTask' => 'content/removeAllCompletedTask',
+    'api/todos/toggleComplete' => 'content/toggleCompleteTask',
+    'api/todos/editTask' => 'content/editTask'
 );
