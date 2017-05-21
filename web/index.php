373502345920f363602712bf5a854a95e8286cb5
J-Veber <kovalsky.corp@gmail.com>
2017-05-21 14:53:43 +0000

    rewrite SITE_PATH

--- web/index.php
+++ web/index.php
@@ -6,16 +6,15 @@ $_SESSION['test'] = "Hello WORLD!";
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+require_once '../App/config.php';
 require_once SITE_PATH . '/vendor/autoload.php';
-require_once SITE_PATH . '/App/config.php';
 require_once SITE_PATH . '/App/Autoloader.php';
-require_once SITE_PATH . '/App/Router.php';
+//require_once SITE_PATH . '/App/Router.php';
 
 $autoLoader = new Autoloader();
 spl_autoload_register(array($autoLoader, '__autoload'));
 
 $app = new App();
-
 $router = new Router($app);
 $router->start();
 
